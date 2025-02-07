aac3447e540528cc17a6e6b946e8649790e43c78
Barbara Losonc <barbara@vallic.com>
2025-02-07 14:02:30 +0100

    fixes for phpcs errors

--- src/Plugin/Commerce/Condition/OrderItemMeasurement.php
+++ src/Plugin/Commerce/Condition/OrderItemMeasurement.php
@@ -17,35 +17,35 @@ use Drupal\commerce\Attribute\CommerceCondition;
 )]
 class OrderItemMeasurement extends MeasurementBaseCondition {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function evaluate(EntityInterface $entity) {
-        $this->assertEntity($entity);
-        /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
-        $order_item = $entity;
-        /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $purchased_entity */
-        $purchased_entity = $order_item->getPurchasedEntity();
-        $configuration = $this->configuration['measurements'];
-        if (empty($configuration) || !$purchased_entity) {
-            return FALSE;
-        }
-
-        foreach ($configuration as $values) {
-            $field_name = $values['field_name'];
-            if (!$purchased_entity->hasField($field_name) || $purchased_entity->get($field_name)->isEmpty()) {
-                return FALSE;
-            }
-
-            $order_item_measurement = $purchased_entity->get($field_name)->first()->toMeasurement();
-
-            if (!$this->evaluateMeasurement($order_item_measurement, $values)) {
-                return FALSE;
-            }
-
-        }
-
-        return TRUE;
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(EntityInterface $entity) {
+    $this->assertEntity($entity);
+    /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
+    $order_item = $entity;
+    /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $purchased_entity */
+    $purchased_entity = $order_item->getPurchasedEntity();
+    $configuration = $this->configuration['measurements'];
+    if (empty($configuration) || !$purchased_entity) {
+      return FALSE;
     }
+
+    foreach ($configuration as $values) {
+      $field_name = $values['field_name'];
+      if (!$purchased_entity->hasField($field_name) || $purchased_entity->get($field_name)->isEmpty()) {
+        return FALSE;
+      }
+
+      $order_item_measurement = $purchased_entity->get($field_name)->first()->toMeasurement();
+
+      if (!$this->evaluateMeasurement($order_item_measurement, $values)) {
+        return FALSE;
+      }
+
+    }
+
+    return TRUE;
+  }
 
 }
--- src/Plugin/Commerce/Condition/OrderItemTotalMeasurement.php
+++ src/Plugin/Commerce/Condition/OrderItemTotalMeasurement.php
@@ -17,50 +17,50 @@ use Drupal\commerce\Attribute\CommerceCondition;
 )]
 class OrderItemTotalMeasurement extends MeasurementBaseCondition {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function evaluate(EntityInterface $entity) {
-        $this->assertEntity($entity);
-        /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
-        $order = $entity;
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(EntityInterface $entity) {
+    $this->assertEntity($entity);
+    /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
+    $order = $entity;
 
-        $order_items = $order->getItems();
-        $configuration = $this->configuration['measurements'];
-        if (empty($configuration) || !$order_items) {
-            return FALSE;
-        }
-
-        /** @var \Drupal\physical\Measurement[] $total_measurement */
-        $total_measurement = [];
-        foreach ($order_items as $order_item) {
-            /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $purchased_entity */
-            $purchased_entity = $order_item->getPurchasedEntity();
-            foreach ($configuration as $values) {
-                $field_name = $values['field_name'];
-                if (!$purchased_entity->hasField($field_name) || $purchased_entity->get($field_name)->isEmpty()) {
-                    return FALSE;
-                }
-                $order_item_measurement = $purchased_entity->get($field_name)->first()->toMeasurement()->multiply($order_item->getQuantity());
-                if (empty($total_measurement[$field_name])) {
-                    $total_measurement[$field_name] = [
-                        'measurement' => $order_item_measurement,
-                        'values' => $values,
-                    ];
-                }
-                else {
-                    $total_measurement[$field_name]['measurement'] = $total_measurement[$field_name]['measurement']->add($order_item_measurement);
-                }
-            }
-        }
-
-        foreach ($total_measurement as $item) {
-            if (!$this->evaluateMeasurement($item['measurement'], $item['values'])) {
-                return FALSE;
-            }
-        }
-
-        return TRUE;
+    $order_items = $order->getItems();
+    $configuration = $this->configuration['measurements'];
+    if (empty($configuration) || !$order_items) {
+      return FALSE;
     }
+
+    /** @var \Drupal\physical\Measurement[] $total_measurement */
+    $total_measurement = [];
+    foreach ($order_items as $order_item) {
+      /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $purchased_entity */
+      $purchased_entity = $order_item->getPurchasedEntity();
+      foreach ($configuration as $values) {
+        $field_name = $values['field_name'];
+        if (!$purchased_entity->hasField($field_name) || $purchased_entity->get($field_name)->isEmpty()) {
+          return FALSE;
+        }
+        $order_item_measurement = $purchased_entity->get($field_name)->first()->toMeasurement()->multiply($order_item->getQuantity());
+        if (empty($total_measurement[$field_name])) {
+          $total_measurement[$field_name] = [
+            'measurement' => $order_item_measurement,
+            'values' => $values,
+          ];
+        }
+        else {
+          $total_measurement[$field_name]['measurement'] = $total_measurement[$field_name]['measurement']->add($order_item_measurement);
+        }
+      }
+    }
+
+    foreach ($total_measurement as $item) {
+      if (!$this->evaluateMeasurement($item['measurement'], $item['values'])) {
+        return FALSE;
+      }
+    }
+
+    return TRUE;
+  }
 
 }
